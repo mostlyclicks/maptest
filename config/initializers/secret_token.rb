785a7ba71d2ada47754bc10cc447d72ac441cdd8
mostlyclicks <carlos@mostlyclicks.com>
2012-09-17 21:24:30 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Maptest::Application.config.secret_token = '7f19f1d3b6d2f2a6082bcf06d3677ade44c2c3562cb52cbe8819433176706e2590253d9c6d694b53d78efcf681674b2b3451f530975c17972ab13088b87eed5e'
